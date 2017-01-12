337ae19ecaed8eafe394e6751d9c1e1b14b7807d
SuperApollo <wpb504@126.com>
2017-01-12 15:30:16 +0800

    加入butterknife

--- app/src/main/java/com/apollo/apollochat/ui/activity/BaseActivity.java
+++ app/src/main/java/com/apollo/apollochat/ui/activity/BaseActivity.java
@@ -1,17 +1,46 @@
 package com.apollo.apollochat.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.ViewConfiguration;
 
+import com.apollo.apollochat.ui.utils.ToastUtils;
+
 import java.lang.reflect.Field;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by zayh_yf20160909 on 2017/1/11.
  */
 
-public class BaseActivity extends FragmentActivity {
+public abstract class BaseActivity extends FragmentActivity {
 
+    protected ToastUtils mToastUtils;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+        setOverflowShowingAlways();
+        initView();
+        mToastUtils = ToastUtils.shareInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(getMenuLayoutId(), menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 使overflowbutton始终显示
+     */
     private void setOverflowShowingAlways() {
         try {
             ViewConfiguration config = ViewConfiguration.get(this);
@@ -22,4 +51,23 @@ public class BaseActivity extends FragmentActivity {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 获取布局id
+     *
+     * @return 布局id
+     */
+    protected abstract int getLayoutId();
+
+    /**
+     * 获取actionbar的menu布局
+     *
+     * @return menu布局资源id
+     */
+    protected abstract int getMenuLayoutId();
+
+    /**
+     * 初始化view
+     */
+    protected abstract void initView();
 }
---
+++ app/src/main/java/com/apollo/apollochat/ui/activity/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.apollo.apollochat.ui.activity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.apollo.apollochat.R;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by zayh_yf20160909 on 2017/1/12.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @BindView(R.id.et_login_username)
+    EditText mEtUserName;
+    @BindView(R.id.et_login_password)
+    EditText mEtPassword;
+    @BindView(R.id.cb_login_remember)
+    CheckBox mCbRemember;
+    @BindView(R.id.btn_login_register)
+    Button mBtnRegister;
+    @BindView(R.id.btn_login_login)
+    Button mBtnLogin;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected int getMenuLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    /**
+     * 绑定button的点击事件，button绑定的方法名自定
+     *
+     * @param v
+     */
+    @OnClick({R.id.btn_login_register, R.id.btn_login_login})
+    public void doClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login_register:
+                mToastUtils.show(this, "注册");
+                break;
+            case R.id.btn_login_login:
+                mToastUtils.show(this, "登录");
+                break;
+        }
+    }
+}
